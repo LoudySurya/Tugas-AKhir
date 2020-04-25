5d046a9459faf7d8a7e76b385122fdffbb74615a
LoudySurya <63911353+LoudySurya@users.noreply.github.com>
2020-04-25 11:40:44 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<script src="http://localhost/covid/aset/jquery.js"></script>
+<script src="http://localhost/covid/aset/bootstrap/js/bootstrap.min.js"></script>
+<br>
+<br>
+
+    <div class="container" style="padding: 30px;">
+        <p align="center">@copyright 2020 - by: PsyDragg  <a href="#">Back to Top</a></p>
+    </div>
+
+</body>
+</html>
